3f9d2419315257fcf229a103e4f66ea4711d4e73
LouiseOdier <maarit.vilen@helsinki.fi>
2025-11-07 17:55:07 +0200

    nhl

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,45 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Oliot:")
+
+    #player_dict ={}
+    fin_players = []
+
+    for player in players:
+        if player.nationality == "FIN":
+            #print(f"{player.name} team {player.team} goals {player.goals} assists {player.assists} total {player.total}")
+            #fin_player = Player(player)
+            fin_players.append(player)
+
+    
+    for player in fin_players:
+        #if player.nationality == "FIN":
+        print(f"{player.name} team {player.team} goals {player.goals} assists {player.assists} total {player.total}")
+
+    fin_players.sort(key=lambda p: p.total, reverse=True)
+    for player in fin_players:
+        #if player.nationality == "FIN":
+        print(f"{player.name} \t {player.team} \t {player.goals} + {player.assists} = {player.total}")
+
+    #print(fin_players)
+
+    #print(player.print_players())
 
 if __name__ == "__main__":
     main()
+
+#Juuso Välimäki team UTA  goals 2 assists 3
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,31 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.team = dict['team']
+        self.nationality = dict['nationality']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.total = dict['goals']+dict['assists']
+    #    self.player_list = []
+    
+    #def players_list(self, player):
+    #    self.player_list.append(player)
+    #    print(player)
+    
+    def print_players(self):
+        print(self.player_list)
+        return self.player_list
+
+    def sort(self, players):
+        pass
+
     
     def __str__(self):
         return self.name
+
+class PlayerReader:
+    pass
+class PlayerStats:
+
+    def top_scorers_by_nationality():
+        pass
